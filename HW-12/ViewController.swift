22975506333e1cd042fc0272c2c2aad934a3799e
fomka94 <fom-aina@mail.ru>
2022-04-27 21:40:23 +0900

    Initial Commit

---
+++ HW-12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW-12
+//
+//  Created by Фомина Айина Валерьевна on 27.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HW-12UITests/HW_12UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HW_12UITestsLaunchTests.swift
+//  HW-12UITests
+//
+//  Created by Фомина Айина Валерьевна on 27.04.2022.
+//
+
+import XCTest
+
+class HW_12UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
